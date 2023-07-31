d7d958accb3a8806127605eb25e6dac25debed11
csangeeth <118877883+csangeeth@users.noreply.github.com>
2023-07-31 12:46:57 +0530

    Update TestBase.java

--- src/test/java/util/TestBase.java
+++ src/test/java/util/TestBase.java
@@ -13,5 +13,18 @@ public class TestBase {
     @BeforeMethod
     public void beforeTest() {
         //LoggerUtil.logINFO("TestClass Running " + this.getClass().toString());
+           try (InputStream inputStream = Example.class.getResourceAsStream("/banner.txt")) {
+            if (inputStream != null) {
+                Scanner scanner = new Scanner(inputStream);
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    System.out.println(line);
+                }
+            } else {
+                System.out.println("Banner file not found.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading banner file: " + e.getMessage());
+        }
     }
 }
